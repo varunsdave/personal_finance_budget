6ffc4ac82cdd7222d669870a86bea7debded5a96
varunsdave <varun.s.dave.public@gmail.com>
2019-06-30 10:26:56 -0400

    Add api operation to balance transaction

--- src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
+++ src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
@@ -2,6 +2,8 @@ package com.varunsdave.personafinance.budget.controller;
 
 import com.varunsdave.personafinance.budget.model.Transaction;
 import com.varunsdave.personafinance.budget.service.transactions.TransactionService;
+import io.swagger.annotations.ApiOperation;
+import jdk.jfr.Description;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,7 @@ public class BalanceController {
      * @return Transaction record of the balance amount
      */
     @PostMapping("")
+    @ApiOperation("Creates a record of new balance type of transaction.")
     public ResponseEntity<Transaction> createBalance(@RequestBody int balanceAmount) {
         return ResponseEntity.ok().body( transactionService.create(balanceAmount, TYPE));
     }
@@ -31,6 +34,7 @@ public class BalanceController {
      * @return List of balance transaction updates
      */
     @GetMapping("balanceTransactions")
+    @ApiOperation("Retrieves a list of all balance updates.")
     public List<Transaction> getAllBalanceTransactions() {
         return transactionService.getTransactionsByType(TYPE);
     }
@@ -40,6 +44,7 @@ public class BalanceController {
      * @return an amount representing current transaction
      */
     @GetMapping("/currentBalance")
+    @ApiOperation("Returns the current balance based on previous transactions")
     public double currentBalance() {
         return transactionService.getCurrentBalance();
     }
