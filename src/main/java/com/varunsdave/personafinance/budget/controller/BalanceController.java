2ad525ca9ba5dd02d0ec369dca23736e1bd55a28
varunsdave <v2dave@uwaterloo.ca>
2019-06-30 10:17:06 -0400

    Document balance controller class methods

--- src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
+++ src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
@@ -15,16 +15,30 @@ public class BalanceController {
     private final TransactionService transactionService;
     private final String TYPE = "balance";
 
+    /**
+     * Creates a record of new balance type of transaction. This is used to update
+     * a balance for a given date.
+     * @param balanceAmount the new balance amount
+     * @return Transaction record of the balance amount
+     */
     @PostMapping("")
     public ResponseEntity<Transaction> createBalance(@RequestBody int balanceAmount) {
         return ResponseEntity.ok().body( transactionService.create(balanceAmount, TYPE));
     }
 
+    /**
+     * Retrieves a list of all balance updates.
+     * @return List of balance transaction updates
+     */
     @GetMapping("balanceTransactions")
     public List<Transaction> getAllBalanceTransactions() {
         return transactionService.getTransactionsByType(TYPE);
     }
 
+    /**
+     * Returns the current balance based on previous transactions
+     * @return an amount representing current transaction
+     */
     @GetMapping("/currentBalance")
     public double currentBalance() {
         return transactionService.getCurrentBalance();
