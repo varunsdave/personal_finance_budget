b0c08605f6911cc9fe342dc618c1629edddc3d2a
varunsdave <varun.s.dave.public@gmail.com>
2019-06-30 10:35:30 -0400

    Add api operation to balance transaction

--- src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
+++ src/main/java/com/varunsdave/personafinance/budget/controller/BalanceController.java
@@ -3,7 +3,6 @@ package com.varunsdave.personafinance.budget.controller;
 import com.varunsdave.personafinance.budget.model.Transaction;
 import com.varunsdave.personafinance.budget.service.transactions.TransactionService;
 import io.swagger.annotations.ApiOperation;
-import jdk.jfr.Description;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
