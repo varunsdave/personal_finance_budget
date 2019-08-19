2a7a6d602abb0e9aec0f98ec27f0e00e1af83995
varunsdave <v2dave@uwaterloo.ca>
2019-08-18 21:06:43 -0400

    fix gradle

--- src/main/java/com/varunsdave/personafinance/budget/BudgetApplication.java
+++ src/main/java/com/varunsdave/personafinance/budget/BudgetApplication.java
@@ -2,9 +2,16 @@ package com.varunsdave.personafinance.budget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BudgetApplication {
+public class BudgetApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BudgetApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BudgetApplication.class, args);
